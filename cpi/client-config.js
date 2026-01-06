1282fc6e3b7af0ab90801b1926e37e8b1ef331ff
andresmtzc <andresmtzc@gmail.com>
2026-01-05 19:26:51 -0600

    senterra brown

--- cpi/client-config.js
+++ cpi/client-config.js
@@ -133,7 +133,7 @@ const CLIENT_CONFIGS = {
     // All colors used throughout the site for easy customization
     colors: {
       // Primary brand colors
-      primary: '#9ea516',        // Orange - main CTA and accent color
+      primary: '#9f7045',        // Orange - main CTA and accent color
       primaryLight: '#ff6b6b',   // Coral - view cone outlines
       cream: '#fcfaf3',          // Cream/Beige - backgrounds and button text
 
